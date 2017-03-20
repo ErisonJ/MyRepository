913538725453f2a4f94c4d92990d1e2944ed429b
ErisonJ <erisondelacruz@gmail.com>
2017-03-20 14:50:35 -0400

    SeleniumTestAmazon

---
+++ TestingSomeWebside/TestingSomeWebside/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+
+namespace TestingSomeWebside
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating object from google chrome
+            IWebDriver driver = new ChromeDriver();
+
+            //going throught the amazon web page
+            Utilities.GotoUrl(driver,"https://www.amazon.com");
+            
+            //login
+            Utilities.Click(driver, "nav-link-accountList", "Id");
+            Utilities.EnterText(driver, "email", "ericksondelacruz@hotmail.com", "Name");
+            Utilities.EnterText(driver, "password", "delacruz17", "Name");
+            Utilities.Click(driver, "signInSubmit", "Id");
+
+            //searching the article to buy
+            Utilities.EnterText(driver, "field-keywords", "samgung  galaxy tab 3", "Name");
+            Utilities.Click(driver, "nav-input", "Class");
+
+            //selecting the article
+            var res= Utilities.GetElementss(driver, "cfMarker", "Class", 2);
+            res.Click();
+
+            //adding to cart and going to the shiping info
+            Utilities.Click(driver, "add-to-cart-button-ubb", "Id");
+            Utilities.Click(driver, "hlb-ptc-btn-native", "Id");
+
+            //filling out the form for the shipping
+            Utilities.EnterText(driver, "enterAddressFullName", "Erison De La Cruz", "Name");
+            Utilities.EnterText(driver, "enterAddressAddressLine1", "Calle 3 #1", "Name");
+            Utilities.EnterText(driver, "enterAddressCity", "Santiago", "Name");
+            Utilities.EnterText(driver, "enterAddressStateOrRegion", "Santiago de los caballeros", "Name");
+            Utilities.EnterText(driver, "enterAddressPostalCode", "51031", "Name");
+            Utilities.EnterText(driver, "enterAddressPhoneNumber", "809-513-4923", "Name");
+            Utilities.DropDown(driver, "enterAddressCountryCode", "Dominican Republic", "Name");
+            //click continue
+            Utilities.Click(driver, "shipToThisAddress", "Name");
+
+        }
+        
+    }
+}
---
+++ TestingSomeWebside/TestingSomeWebside/Utilities.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+
+namespace TestingSomeWebside
+{
+    class Utilities
+    {
+        public static void EnterText(IWebDriver driver, string element, string value, string elementType)
+        {
+            if (elementType == "Id")
+            {
+                driver.FindElement(By.Id(element)).SendKeys(value);
+            }
+            else if (elementType == "Name")
+            {
+                driver.FindElement(By.Name(element)).SendKeys(value);
+
+            }
+
+        }
+        
+        public static IWebElement GetElement(IWebDriver driver, string element, string elementType) {
+
+            IWebElement Got = null;
+
+            if (elementType == "Class")
+            {
+                Got = driver.FindElement(By.ClassName(element));
+
+            }
+            else if (elementType == "Id") {
+
+                Got = driver.FindElement(By.Id(element));
+            }
+            else if (elementType == "Name")
+            {
+
+                Got = driver.FindElement(By.ClassName(element));
+            }
+
+            return Got;
+        }
+
+        public static IWebElement GetElementss(IWebDriver driver, string element, string elementType,int pos)
+        {
+
+            IWebElement Got = null;
+
+            if (elementType == "Class")
+            {
+                Got = driver.FindElements(By.ClassName(element))[pos];
+
+            }
+            else if (elementType == "Id")
+            {
+
+                Got = driver.FindElements(By.Id(element))[pos];
+            }
+            else if (elementType == "Name")
+            {
+
+                Got = driver.FindElements(By.ClassName(element))[pos];
+            }
+
+            return Got;
+        }
+
+        public static void GotoUrl(IWebDriver driver ,string url) {
+
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+        }
+
+        public static void Click(IWebDriver driver, string element, string elementType)
+        {
+            try
+            {
+                if (elementType == "Id")
+                {
+                    driver.FindElement(By.Id(element)).Click();
+                }
+                else if (elementType == "Name")
+                {
+                    driver.FindElement(By.Name(element)).Click();
+
+                }
+                else if (elementType == "Class")
+                {
+                    driver.FindElement(By.ClassName(element)).Click();
+
+                }
+                else if (elementType == "LinkText")
+                {
+                    driver.FindElement(By.LinkText(element)).Click();
+                }
+
+                else if (elementType == "CssSelector")
+                {
+                    driver.FindElement(By.CssSelector(element)).Click();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        public static void DropDown(IWebDriver driver, string element, string value, string elementType)
+        {
+            if (elementType == "Id")
+            {
+                new SelectElement(driver.FindElement(By.Id(element))).SelectByText(value);
+            }
+            else if (elementType == "Name")
+            {
+                new SelectElement(driver.FindElement(By.Name(element))).SelectByText(value);
+
+            }
+        }
+
+
+    }
+
+}
